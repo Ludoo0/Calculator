ccb6c2e82e8a7ac08b72bc2dd13203133b58579c
Ludoo <ludoo0215@gmail.com>
2024-04-30 21:56:34 +0200

    added delete-function

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ function getResult() {
   let result = eval(document.getElementById("result").innerHTML);
   document.getElementById("result").innerHTML = result;
 }
+function deleteLast() {
+  if (document.getElementById("result").innerHTML.endsWith(" ")) {
+    document.getElementById("result").innerHTML = document
+      .getElementById("result")
+      .innerHTML.slice(0, -3);
+  } else {
+    document.getElementById('result').innerHTML = document.getElementById('result').innerHTML.slice(0, -1);
+  }
+}
